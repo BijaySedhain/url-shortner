68a856a3c0318fe00403fbb91ef522b6b8bca8b4
Bijay Sedhain <bijaysedhain@gmail.com>
2025-04-24 15:11:19 -0500

    Initial commit

---
+++ src/main/java/com/java/shortner/SpringBootUrlShortnerApplication.java
@@ -0,0 +1,13 @@
+package com.java.shortner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootUrlShortnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootUrlShortnerApplication.class, args);
+    }
+
+}
