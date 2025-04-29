c1b023b3653ee30fe8779785f3b4f30c53b82247
Bijay Sedhain <bijaysedhain@gmail.com>
2025-04-29 17:14:10 -0500

    Optimize query in findPublicShortUrls by using @EntityGraph to eagerly fetch createdBy and avoid N+1 problem.

--- src/main/java/com/java/shortener/repository/ShortUrlRepository.java
+++ src/main/java/com/java/shortener/repository/ShortUrlRepository.java
@@ -2,6 +2,7 @@ package com.java.shortener.repository;
 
 import com.java.shortener.domain.entities.ShortUrl;
 import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -11,6 +12,17 @@ import java.util.List;
 @Repository
 public interface ShortUrlRepository extends JpaRepository<ShortUrl, Long> {
 
+    /*
+        used the @EntityGraph annotation with attributePaths = "createdBy" to
+        optimize the query by specifying that the createdBy relationship should be eagerly
+        fetched when executing the findPublicShortUrls method. This avoids the N+1 select problem
+        by ensuring that the createdBy association is fetched in a single query, rather than lazily
+        loading it for each ShortUrl entity.
+
+       Could have used @Query("select su from ShortUrl su left join fetch su.createdBy where su.isPrivate = false order by su.createdAt desc ")
+     */
+
     @Query("select su from ShortUrl su where su.isPrivate = false order by su.createdAt desc ")
+    @EntityGraph(attributePaths = "createdBy")
     List<ShortUrl> findPublicShortUrls();
 }
